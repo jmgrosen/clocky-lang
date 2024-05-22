29637870d7f17bc13d40c00c656a862fe42c6644
Jessie Grosen <jmgrosen@gmail.com>
2024-05-22 12:52:59 -0400

    push sketching to github pages

--- sketching/vite.config.js
+++ sketching/vite.config.js
@@ -3,6 +3,7 @@ import wasm from "vite-plugin-wasm";
 import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  base: '/clocky-lang/',
   plugins: [
     wasm(),
     topLevelAwait(),
